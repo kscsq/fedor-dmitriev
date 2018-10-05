5ff3a4ce9efea8c530788d3e00ee7dd9f735ad6b
Fedor Dmitriev <kscsq@mail.ru>
2018-10-05 13:23:17 +0300

    Express added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var express = require("express");
+var express = require("express"),
+    engines = require("consolidate");
+
 var app = express();
 
+app.use(express.static(__dirname + '/'));
+
 app.get("/", function (req, res) {
-    res.render("index.html");
+    res.render('index.html');
 });
 
 app.listen(3000, function () {
-
+    // console.log("Started");
 });
