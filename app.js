c27a56b586d62361424ebb744069a6a6d5447b5e
Fedor Dmitriev <kscsq@mail.ru>
2018-10-05 13:48:19 +0300

    Procfile added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-    engines = require("consolidate");
+    engines = require("consolidate"),
+    http = require("http");
 
 var app = express();
 
@@ -9,6 +10,18 @@ app.get("/", function (req, res) {
     res.render('index.html');
 });
 
-app.listen(3000, function () {
-    // console.log("Started");
-});
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on ${port}`));
